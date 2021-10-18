bef0caa1dc2f4584b57b42d3940287327fbda9ff
Nicolai Willems <nicwi@yousee.dk>
2021-10-18 02:20:12 +0200

    feat: Add exchange view and source info

--- src/components/views/Exchange.vue
+++ src/components/views/Exchange.vue
@@ -2,7 +2,250 @@
 </script>
 
 <template>
-Exchange view
+<div class="h-googleframe overflow-x-scroll">
+<svg class="w-screen"
+ viewBox="0.00 0.00 361.37 1043.19" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<g id="graph0" class="graph" transform="scale(1 1) rotate(0) translate(4 1039.19)">
+<title>G</title>
+<polygon fill="white" stroke="transparent" points="-4,4 -4,-1039.19 357.37,-1039.19 357.37,4 -4,4"/>
+<!-- pind -->
+<g id="node1" class="node">
+<title>pind</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-387.59" rx="50.09" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-383.89" font-family="Times,serif" font-size="14.00">🥢 Pind, 3</text>
+</g>
+<!-- pil -->
+<g id="node13" class="node">
+<title>pil</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-479.59" rx="48.18" ry="48.18"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-479.59" rx="52.19" ry="52.19"/>
+<text text-anchor="middle" x="260.33" y="-475.89" font-family="Times,serif" font-size="14.00">🏹 Pil, 45</text>
+</g>
+<!-- pind&#45;&gt;pil -->
+<g id="edge1" class="edge">
+<title>pind&#45;&gt;pil</title>
+<path fill="none" stroke="black" d="M96.75,-401.97C125.32,-415.61 168.94,-436.44 203.81,-453.09"/>
+<polygon fill="black" stroke="black" points="202.4,-456.29 212.93,-457.44 205.42,-449.97 202.4,-456.29"/>
+</g>
+<!-- kors -->
+<g id="node14" class="node">
+<title>kors</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-350.59" rx="55.47" ry="55.47"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-350.59" rx="59.49" ry="59.49"/>
+<text text-anchor="middle" x="260.33" y="-346.89" font-family="Times,serif" font-size="14.00">✝️ Kors, 35</text>
+</g>
+<!-- pind&#45;&gt;kors -->
+<g id="edge4" class="edge">
+<title>pind&#45;&gt;kors</title>
+<path fill="none" stroke="black" d="M110.37,-379.2C134.39,-374.59 164.71,-368.76 191.72,-363.58"/>
+<polygon fill="black" stroke="black" points="192.72,-366.95 201.89,-361.62 191.4,-360.07 192.72,-366.95"/>
+</g>
+<!-- fjer -->
+<g id="node2" class="node">
+<title>fjer</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-479.59" rx="47.39" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-475.89" font-family="Times,serif" font-size="14.00">🪶 Fjer, 6</text>
+</g>
+<!-- fjer&#45;&gt;pil -->
+<g id="edge2" class="edge">
+<title>fjer&#45;&gt;pil</title>
+<path fill="none" stroke="black" d="M113.29,-479.59C138.67,-479.59 170.39,-479.59 197.78,-479.59"/>
+<polygon fill="black" stroke="black" points="198.04,-483.09 208.04,-479.59 198.04,-476.09 198.04,-483.09"/>
+</g>
+<!-- snor -->
+<g id="node3" class="node">
+<title>snor</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-171.59" rx="50.09" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-167.89" font-family="Times,serif" font-size="14.00">🧵 Snor, 2</text>
+</g>
+<!-- snor&#45;&gt;kors -->
+<g id="edge3" class="edge">
+<title>snor&#45;&gt;kors</title>
+<path fill="none" stroke="black" d="M105.82,-182.33C115.03,-186.28 124.2,-191.57 131.29,-198.59 160.14,-227.18 142.08,-250.75 167.29,-282.59 176.61,-294.36 188.44,-305.11 200.5,-314.41"/>
+<polygon fill="black" stroke="black" points="198.74,-317.46 208.85,-320.61 202.91,-311.84 198.74,-317.46"/>
+</g>
+<!-- sko -->
+<g id="node16" class="node">
+<title>sko</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-217.59" rx="51.99" ry="51.99"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-217.59" rx="55.99" ry="55.99"/>
+<text text-anchor="middle" x="260.33" y="-213.89" font-family="Times,serif" font-size="14.00">🩴 Sko, 55</text>
+</g>
+<!-- snor&#45;&gt;sko -->
+<g id="edge7" class="edge">
+<title>snor&#45;&gt;sko</title>
+<path fill="none" stroke="black" d="M107.98,-181.46C133.5,-187.55 166.77,-195.5 195.61,-202.38"/>
+<polygon fill="black" stroke="black" points="195.07,-205.85 205.61,-204.77 196.69,-199.04 195.07,-205.85"/>
+</g>
+<!-- smykke -->
+<g id="node17" class="node">
+<title>smykke</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-71.59" rx="67.68" ry="67.68"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-71.59" rx="71.69" ry="71.69"/>
+<text text-anchor="middle" x="260.33" y="-67.89" font-family="Times,serif" font-size="14.00">💍 Smykke, 80</text>
+</g>
+<!-- snor&#45;&gt;smykke -->
+<g id="edge10" class="edge">
+<title>snor&#45;&gt;smykke</title>
+<path fill="none" stroke="black" d="M88.19,-155.51C100.9,-149.32 117.21,-142.52 131.29,-135.59 149.28,-126.75 168.25,-116.48 185.92,-106.75"/>
+<polygon fill="black" stroke="black" points="187.96,-109.62 195.02,-101.72 184.58,-103.49 187.96,-109.62"/>
+</g>
+<!-- snor&#45;&gt;smykke -->
+<g id="edge11" class="edge">
+<title>snor&#45;&gt;smykke</title>
+<path fill="none" stroke="black" d="M110.09,-163.18C117.24,-160.22 124.52,-156.92 131.29,-153.59 152.09,-143.37 174.21,-131.24 194.1,-119.43"/>
+<polygon fill="black" stroke="black" points="196,-122.37 202.77,-114.22 192.4,-116.37 196,-122.37"/>
+</g>
+<!-- perle -->
+<g id="node4" class="node">
+<title>perle</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-117.59" rx="53.89" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-113.89" font-family="Times,serif" font-size="14.00">🦪 Perler, 3</text>
+</g>
+<!-- perle&#45;&gt;sko -->
+<g id="edge9" class="edge">
+<title>perle&#45;&gt;sko</title>
+<path fill="none" stroke="black" d="M101.55,-131.26C111.28,-135.31 121.79,-139.93 131.29,-144.59 155.31,-156.4 181.07,-170.75 203.15,-183.58"/>
+<polygon fill="black" stroke="black" points="201.65,-186.76 212.05,-188.79 205.18,-180.72 201.65,-186.76"/>
+</g>
+<!-- perle&#45;&gt;smykke -->
+<g id="edge12" class="edge">
+<title>perle&#45;&gt;smykke</title>
+<path fill="none" stroke="black" d="M109.89,-107.27C130.76,-102.29 156.46,-96.15 180.63,-90.38"/>
+<polygon fill="black" stroke="black" points="181.6,-93.75 190.52,-88.02 179.98,-86.94 181.6,-93.75"/>
+</g>
+<!-- kobber -->
+<g id="node5" class="node">
+<title>kobber</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-63.59" rx="59.59" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-59.89" font-family="Times,serif" font-size="14.00">📿 Kobber, 9</text>
+</g>
+<!-- kobber&#45;&gt;smykke -->
+<g id="edge13" class="edge">
+<title>kobber&#45;&gt;smykke</title>
+<path fill="none" stroke="black" d="M124.98,-66.02C141.82,-66.72 160.5,-67.49 178.45,-68.24"/>
+<polygon fill="black" stroke="black" points="178.48,-71.74 188.62,-68.66 178.77,-64.75 178.48,-71.74"/>
+</g>
+<!-- gummi -->
+<g id="node6" class="node">
+<title>gummi</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-225.59" rx="61.99" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-221.89" font-family="Times,serif" font-size="14.00">🪠 Gummi, 6</text>
+</g>
+<!-- gummi&#45;&gt;sko -->
+<g id="edge8" class="edge">
+<title>gummi&#45;&gt;sko</title>
+<path fill="none" stroke="black" d="M126.82,-223.1C148.17,-222.21 172.3,-221.21 194.06,-220.3"/>
+<polygon fill="black" stroke="black" points="194.35,-223.8 204.19,-219.88 194.06,-216.8 194.35,-223.8"/>
+</g>
+<!-- salt -->
+<g id="node7" class="node">
+<title>salt</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-990.59" rx="47.39" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-986.89" font-family="Times,serif" font-size="14.00">🧂 Salt, 6</text>
+</g>
+<!-- popcorn -->
+<g id="node18" class="node">
+<title>popcorn</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-963.59" rx="67.68" ry="67.68"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-963.59" rx="71.69" ry="71.69"/>
+<text text-anchor="middle" x="260.33" y="-959.89" font-family="Times,serif" font-size="14.00">🍿 Popcorn, 70</text>
+</g>
+<!-- salt&#45;&gt;popcorn -->
+<g id="edge14" class="edge">
+<title>salt&#45;&gt;popcorn</title>
+<path fill="none" stroke="black" d="M110.37,-984.47C130.77,-981.61 155.72,-978.11 179.31,-974.81"/>
+<polygon fill="black" stroke="black" points="179.91,-978.26 189.33,-973.4 178.94,-971.32 179.91,-978.26"/>
+</g>
+<!-- olie -->
+<g id="node8" class="node">
+<title>olie</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-822.59" rx="48.99" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-818.89" font-family="Times,serif" font-size="14.00">⛽ Olie, 2</text>
+</g>
+<!-- myg -->
+<g id="node15" class="node">
+<title>myg</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-780.59" rx="89.08" ry="89.08"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-780.59" rx="93.08" ry="93.08"/>
+<text text-anchor="middle" x="260.33" y="-776.89" font-family="Times,serif" font-size="14.00">🦟 Myggebalsam, 25</text>
+</g>
+<!-- olie&#45;&gt;myg -->
+<g id="edge5" class="edge">
+<title>olie&#45;&gt;myg</title>
+<path fill="none" stroke="black" d="M107.98,-813.58C123.26,-810.25 141.31,-806.32 159.53,-802.35"/>
+<polygon fill="black" stroke="black" points="160.33,-805.76 169.36,-800.21 158.84,-798.92 160.33,-805.76"/>
+</g>
+<!-- olie&#45;&gt;popcorn -->
+<g id="edge15" class="edge">
+<title>olie&#45;&gt;popcorn</title>
+<path fill="none" stroke="black" d="M88.75,-838.77C114.2,-857.4 157.21,-888.86 193.67,-915.54"/>
+<polygon fill="black" stroke="black" points="191.68,-918.43 201.82,-921.51 195.81,-912.78 191.68,-918.43"/>
+</g>
+<!-- krydderi -->
+<g id="node9" class="node">
+<title>krydderi</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-737.59" rx="65.79" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-733.89" font-family="Times,serif" font-size="14.00">🌿 Krydderi, 3</text>
+</g>
+<!-- krydderi&#45;&gt;myg -->
+<g id="edge6" class="edge">
+<title>krydderi&#45;&gt;myg</title>
+<path fill="none" stroke="black" d="M117.02,-748.84C130.06,-751.75 144.58,-754.99 159.21,-758.25"/>
+<polygon fill="black" stroke="black" points="158.82,-761.75 169.34,-760.51 160.35,-754.92 158.82,-761.75"/>
+</g>
+<!-- brod -->
+<g id="node19" class="node">
+<title>brod</title>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-609.59" rx="55.8" ry="55.8"/>
+<ellipse fill="none" stroke="black" cx="260.33" cy="-609.59" rx="59.79" ry="59.79"/>
+<text text-anchor="middle" x="260.33" y="-605.89" font-family="Times,serif" font-size="14.00">🥖 Brød, 75</text>
+</g>
+<!-- krydderi&#45;&gt;brod -->
+<g id="edge19" class="edge">
+<title>krydderi&#45;&gt;brod</title>
+<path fill="none" stroke="black" d="M91.83,-720.86C119.51,-702.48 164.87,-672.34 201.41,-648.07"/>
+<polygon fill="black" stroke="black" points="203.62,-650.8 210.02,-642.35 199.75,-644.97 203.62,-650.8"/>
+</g>
+<!-- vand -->
+<g id="node10" class="node">
+<title>vand</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-636.59" rx="51.19" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-632.89" font-family="Times,serif" font-size="14.00">💧 Vand, 6</text>
+</g>
+<!-- vand&#45;&gt;brod -->
+<g id="edge17" class="edge">
+<title>vand&#45;&gt;brod</title>
+<path fill="none" stroke="black" d="M113.78,-629.99C136.99,-626.74 165.42,-622.75 190.96,-619.17"/>
+<polygon fill="black" stroke="black" points="191.55,-622.63 200.97,-617.77 190.58,-615.69 191.55,-622.63"/>
+</g>
+<!-- mel -->
+<g id="node11" class="node">
+<title>mel</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-582.59" rx="48.19" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-578.89" font-family="Times,serif" font-size="14.00">🍞 Mel, 6</text>
+</g>
+<!-- mel&#45;&gt;brod -->
+<g id="edge18" class="edge">
+<title>mel&#45;&gt;brod</title>
+<path fill="none" stroke="black" d="M110.85,-588.79C134.5,-592.1 164.14,-596.26 190.7,-599.98"/>
+<polygon fill="black" stroke="black" points="190.33,-603.46 200.72,-601.38 191.3,-596.53 190.33,-603.46"/>
+</g>
+<!-- majs -->
+<g id="node12" class="node">
+<title>majs</title>
+<ellipse fill="none" stroke="black" cx="65.64" cy="-936.59" rx="51.99" ry="18"/>
+<text text-anchor="middle" x="65.64" y="-932.89" font-family="Times,serif" font-size="14.00">🌽 Majs, 6</text>
+</g>
+<!-- majs&#45;&gt;popcorn -->
+<g id="edge16" class="edge">
+<title>majs&#45;&gt;popcorn</title>
+<path fill="none" stroke="black" d="M114.27,-943.27C133.89,-946.02 157.17,-949.28 179.27,-952.38"/>
+<polygon fill="black" stroke="black" points="178.93,-955.86 189.32,-953.78 179.9,-948.93 178.93,-955.86"/>
+</g>
+</g>
+</svg>
+</div>
 </template>
 
 <style scoped>
